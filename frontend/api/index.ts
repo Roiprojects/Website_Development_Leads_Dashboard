6e1edc5330609a218ecb0d50248dcb6205c56fe6
Roiprojects <roiprojects@gmail.com>
2026-04-21 10:10:49 +0530

    cleanup: remove duplicated frontend subfolder to consolidate on root project

--- frontend/api/index.ts
+++
@@ -1,9 +0,0 @@
-import app from '../backend/src/index';
-
-export default function(req: any, res: any) {
-  try {
-    return app(req, res);
-  } catch (err: any) {
-    return res.status(200).json({ status: 'caught error', message: err.message, stack: err.stack });
-  }
-}
